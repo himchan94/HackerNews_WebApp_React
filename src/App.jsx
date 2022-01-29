4a5421447ee03b9a3c8387c39a201baec6a44222
himchan94 <himchan9494@gmail.com>
2022-01-29 19:26:13 +0900

    feat: Created assets index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { DailyNewsCard, Dummy, InputArea } from "./components";
 import { Routes, Route, NavLink } from "react-router-dom";
-import { DailyNewsContainer } from "./container";
+import { DailyNewsContainer, PostContainer } from "./container";
 const App = () => {
   return (
     <>
       <Dummy />
       <InputArea />
       <DailyNewsContainer />
+      <PostContainer />
       <Routes></Routes>
     </>
   );
