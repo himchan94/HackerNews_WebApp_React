2e374812eaafa4b7f409a940f3eddfdad4a4e3ae
himchan94 <himchan9494@gmail.com>
2022-02-02 14:14:36 +0900

    feat: Updated elements

--- src/elements/index.js
+++ src/elements/index.js
@@ -3,3 +3,4 @@ export { default as ImageBox } from "./ImageBox";
 export { default as Input } from "./Input";
 export { default as Karma } from "./Karma";
 export { default as Typhography } from "./Typhography";
+export { default as Button } from "./Button";
